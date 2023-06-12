69db026dc0cbdf3e7092688ec4d8fafb51cdaa8b
JuliMontes <jamontesg@uqvirtual.edu.co>
2023-06-11 20:46:57 -0500

    Agregando archivo con variables de color y font-weight

---
+++ src/components/UI/variablesStyle.js
@@ -0,0 +1,23 @@
+export const colorPrimary = '#6B5BE2'
+
+export const colorUrbano = '#00C86F'
+export const colorSalsa = '#6B5BE2'
+export const colorVallenato = '#FFBA05'
+export const colorPop = '#6BD1FF'
+export const colorBalada = '#DC6EBE'
+
+export const colorBlack = '#000000'
+export const colorBlackMedium = '#4A4A4A'
+export const colorBlackLight = '#5C5C5C'
+export const colorBlackLighter = '#9E9E9E'
+export const colorWhite = '#F5F5F5'
+
+export const colorGray = '#C2C2C2'
+export const colorGrayMedium = '#E5E5E5'
+
+export const colorRed = '#C62828'
+export const colorRedDark = '#BE3144'
+export const colorPink = '#FCE7E7'
+
+export const fontWeightNormal = '400'
+export const fontWeightLight = '300'
