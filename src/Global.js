a80951873de1a8e17707d7116e6bf6f30a5a85c5
JuliMontes <jamontesg@uqvirtual.edu.co>
2023-06-11 20:47:57 -0500

    Agregando estilos globales con styled-components

---
+++ src/Global.js
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components'
+import {
+  colorBlack,
+  colorBlackLight,
+  colorPrimary,
+  colorRedDark,
+  fontWeightLight,
+  fontWeightNormal
+} from './components/UI/variablesStyle'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Roboto', sans-serif;
+    background: linear-gradient(145deg, ${colorPrimary} 0%, ${colorBlack} 70%);
+  }
+
+  h1 {
+    font-size: 3.75rem;
+    font-weight: ${fontWeightNormal};
+  }
+
+  h2 {
+    font-size: 2.5rem;
+    font-weight: ${fontWeightNormal};
+  }
+
+  h3 {
+    font-size: 1.875rem;
+    font-weight: ${fontWeightNormal};
+  }
+
+  h4 {
+    font-size: 1.688rem;
+    font-weight: ${fontWeightLight};
+  }
+
+  p {
+    font-size: 1.125rem;
+    font-weight: ${fontWeightLight};
+  }
+
+  span {
+    font-size: 1rem;
+    font-weight: ${fontWeightLight};
+  }
+`
