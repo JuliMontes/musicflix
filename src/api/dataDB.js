82eb9fdb66e4bdf7617823f5aaa04fa9f36f8ebc
JuliMontes <jamontesg@uqvirtual.edu.co>
2023-06-12 14:10:06 -0500

    Agregando un enlace a la fake-api

--- src/api/dataDB.js
+++ src/api/dataDB.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const dataDB = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://musicflix-api.vercel.app/'
+  // baseURL: 'http://localhost:5000'
 })
 
 export const getVideos = () => dataDB.get('/videos')
